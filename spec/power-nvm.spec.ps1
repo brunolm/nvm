7a059f79998d538545d2a87389acd7ab95c9fd79
Bruno Leonardo Michels <brunolm7@gmail.com>
2017-08-19 00:42:14 -0300

    Refactor tests

--- spec/power-nvm.spec.ps1
+++ spec/power-nvm.spec.ps1
@@ -12,61 +12,71 @@ InModuleScope power-nvm {
             );
         }
 
-        Context "When called without args" {
+        Context "when called without args" {
             It "should list all installed folders" {
-                (nvm ls).Version -contains "v8.4.1" | Should Be $true
-                (nvm ls).Version -contains "v8.4.0" | Should Be $true
-                (nvm ls).Version -contains "v8.3.0" | Should Be $true
-                (nvm ls).Version -contains "v0.8.0" | Should Be $true
-                (nvm ls).Version -contains "v7.10.0" | Should Be $true
+                $actual = nvm ls;
+
+                $actual.Version -contains "v8.4.1" | Should Be $true
+                $actual.Version -contains "v8.4.0" | Should Be $true
+                $actual.Version -contains "v8.3.0" | Should Be $true
+                $actual.Version -contains "v0.8.0" | Should Be $true
+                $actual.Version -contains "v7.10.0" | Should Be $true
 
                 Assert-MockCalled Get-ChildItem -Times 1
             }
         }
 
-        Context "When called with Filter=v8" {
+        Context "when called with Filter=v8" {
             It "should list installed folders matching v8" {
-                (nvm ls "v8").Version -contains "v8.4.1" | Should Be $true
-                (nvm ls "v8").Version -contains "v8.4.0" | Should Be $true
-                (nvm ls "v8").Version -contains "v8.3.0" | Should Be $true
-                (nvm ls "v8").Version -contains "v0.8.0" | Should Be $false
-                (nvm ls "v8").Version -contains "v7.10.0" | Should Be $false
+                $actual = nvm ls "v8";
+
+                $actual.Version -contains "v8.4.1" | Should Be $true
+                $actual.Version -contains "v8.4.0" | Should Be $true
+                $actual.Version -contains "v8.3.0" | Should Be $true
+                $actual.Version -contains "v0.8.0" | Should Be $false
+                $actual.Version -contains "v7.10.0" | Should Be $false
 
                 Assert-MockCalled Get-ChildItem -Times 1
             }
         }
 
-        Context "When called with Filter=8" {
+        Context "when called with Filter=8" {
             It "should list installed folders matching 8" {
-                (nvm ls "8").Version -contains "v8.4.1" | Should Be $true
-                (nvm ls "8").Version -contains "v8.4.0" | Should Be $true
-                (nvm ls "8").Version -contains "v8.3.0" | Should Be $true
-                (nvm ls "8").Version -contains "v0.8.0" | Should Be $false
-                (nvm ls "8").Version -contains "v7.10.0" | Should Be $false
+                $actual = nvm ls "8";
+
+                $actual.Version -contains "v8.4.1" | Should Be $true
+                $actual.Version -contains "v8.4.0" | Should Be $true
+                $actual.Version -contains "v8.3.0" | Should Be $true
+                $actual.Version -contains "v0.8.0" | Should Be $false
+                $actual.Version -contains "v7.10.0" | Should Be $false
 
                 Assert-MockCalled Get-ChildItem -Times 1
             }
         }
 
-        Context "When called with Filter=8.4" {
+        Context "when called with Filter=8.4" {
             It "should list installed folders matching 8.4" {
-                (nvm ls "8.4").Version -contains "v8.4.1" | Should Be $true
-                (nvm ls "8.4").Version -contains "v8.4.0" | Should Be $true
-                (nvm ls "8.4").Version -contains "v8.3.0" | Should Be $false
-                (nvm ls "8.4").Version -contains "v0.8.0" | Should Be $false
-                (nvm ls "8.4").Version -contains "v7.10.0" | Should Be $false
+                $actual = nvm ls "8.4";
+
+                $actual.Version -contains "v8.4.1" | Should Be $true
+                $actual.Version -contains "v8.4.0" | Should Be $true
+                $actual.Version -contains "v8.3.0" | Should Be $false
+                $actual.Version -contains "v0.8.0" | Should Be $false
+                $actual.Version -contains "v7.10.0" | Should Be $false
 
                 Assert-MockCalled Get-ChildItem -Times 1
             }
         }
 
-        Context "When called with Filter=8.4.0" {
+        Context "when called with Filter=8.4.0" {
             It "should list installed folders matching 8.4.0" {
-                (nvm ls "8.4.0").Version -contains "v8.4.1" | Should Be $false
-                (nvm ls "8.4.0").Version -contains "v8.4.0" | Should Be $true
-                (nvm ls "8.4.0").Version -contains "v8.3.0" | Should Be $false
-                (nvm ls "8.4.0").Version -contains "v0.8.0" | Should Be $false
-                (nvm ls "8.4.0").Version -contains "v7.10.0" | Should Be $false
+                $actual = nvm ls "8.4.0";
+
+                $actual.Version -contains "v8.4.1" | Should Be $false
+                $actual.Version -contains "v8.4.0" | Should Be $true
+                $actual.Version -contains "v8.3.0" | Should Be $false
+                $actual.Version -contains "v0.8.0" | Should Be $false
+                $actual.Version -contains "v7.10.0" | Should Be $false
 
                 Assert-MockCalled Get-ChildItem -Times 1
             }
